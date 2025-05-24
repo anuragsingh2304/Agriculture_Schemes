9a762f69c07dca2e864620ec2a30f116d9f97d4f
AtulMishra001 <atulm7151@gmail.com>
2025-05-24 17:57:53 +0530

    admin login changed

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -20,7 +20,7 @@ export default function AdminLogin() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/api/auth/admin/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/auth/admin/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
